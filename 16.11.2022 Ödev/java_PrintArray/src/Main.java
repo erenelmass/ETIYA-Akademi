9081ebf4e932088411e89bda8aafee6ee204b18b
eren <84076408+erenelmass@users.noreply.github.com>
2022-11-16 16:15:34 +0300

    Add files via upload

---
+++ 16.11.2022 Ödev/java_PrintArray/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] List = {"Ankara", "Bursa", "Edirne", "Ağrı", "Kars", "İzmit"};
+
+        for (int i = 0; i < List.length; i++)
+        {
+            System.out.println(List[i]);
+        }
+    }
+}
